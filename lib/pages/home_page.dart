0d9ed147d2172ba94a2fcac80eca3c6c61f53e62
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-10-30 21:42:54 -0500

    primera forma de definir bordes circulares en un container

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,7 +9,25 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.black,
       appBar: AppBar(title: Text("Dice App G14"), centerTitle: true),
       body: Center(
-        child: Column(children: [Image.asset("assets/images/dice1.png")]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                // BORDES REDONDEADOS
+                // primera forma
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Image.asset(
+                "assets/images/dice1.png",
+                height: 200,
+                width: 200,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
