27e0185278e34799667c1b5bbb48b3c7867f4fde
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-11-05 21:54:20 -0500

    wrap

--- lib/pages/deports_favorites_page.dart
+++ lib/pages/deports_favorites_page.dart
@@ -1,7 +1,36 @@
+import 'package:diceappg14/models/deport_model.dart';
 import 'package:flutter/material.dart';
 
 class DeportsFavoritesPage extends StatelessWidget {
   TextStyle titleStyle = TextStyle(fontSize: 23, fontWeight: FontWeight.bold);
+
+  Widget _buildDeportButtonContainer() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        // color: Colors.red,
+        border: Border.all(width: 2, color: Colors.orange),
+      ),
+      child: Text("Gimnasio", style: TextStyle(fontSize: 19)),
+    );
+  }
+
+  Widget _buildDeportButton(DeportModel deportModel) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text(deportModel.nombre, style: TextStyle(fontSize: 17)),
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(10),
+          side: BorderSide(color: Colors.orange, width: 2),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,28 +47,14 @@ class DeportsFavoritesPage extends StatelessWidget {
           children: [
             Text("¿Cuáles son tus deportes favoritos?", style: titleStyle),
 
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Baloncesto", style: TextStyle(fontSize: 17)),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadiusGeometry.circular(10),
-                  side: BorderSide(color: Colors.orange, width: 2),
-                ),
-              ),
-            ),
-
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                // color: Colors.red,
-                border: Border.all(width: 2, color: Colors.orange),
-              ),
-              child: Text("Gimnasio", style: TextStyle(fontSize: 19)),
+            Wrap(
+              spacing: 16, //espaciado horizontal entre elementos
+              runSpacing: 5, //espaciado vertical entre filas
+              alignment: WrapAlignment.spaceAround,
+              children: [
+                for (int i = 0; i < deportList.length; i++)
+                  _buildDeportButton(deportList[i]),
+              ],
             ),
           ],
         ),
