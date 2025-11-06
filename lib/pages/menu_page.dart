c58a768ec77dbae9b16c8cd637d24e9ee608c77f
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-11-05 20:18:41 -0500

    mapas

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,7 +1,99 @@
 import 'package:flutter/material.dart';
 
 class MenuPage extends StatelessWidget {
-  const MenuPage({super.key});
+  MenuPage({super.key});
+
+  final List<Map<String, dynamic>> _menuList = [
+    {
+      "id": 1,
+      "days": "Lun - Mar -Mie",
+      "price": 5,
+      "imageUrl":
+          "https://images.pexels.com/photos/28503598/pexels-photo-28503598.jpeg",
+    },
+    {
+      "id": 2,
+      "days": "Lun - Mar",
+      "price": 7,
+      "imageUrl":
+          "https://images.pexels.com/photos/33432683/pexels-photo-33432683.jpeg?",
+    },
+    {
+      "id": 3,
+      "days": "Jue - Vier",
+      "price": 6.5,
+      "imageUrl":
+          "https://images.pexels.com/photos/291528/pexels-photo-291528.jpeg",
+    },
+    {
+      "id": 4,
+      "days": "Sab",
+      "price": 9,
+      "imageUrl":
+          "https://images.pexels.com/photos/6163259/pexels-photo-6163259.jpeg",
+    },
+    {
+      "id": 5,
+      "days": "Dom",
+      "price": 10,
+      "imageUrl":
+          "https://images.pexels.com/photos/539451/pexels-photo-539451.jpeg",
+    },
+  ];
+
+  Widget _buildMenuCard(double screenWidth, Map<String, dynamic> menuMap) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 8),
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        border: Border.all(width: 2, color: Colors.black),
+      ),
+      child: Row(
+        children: [
+          // PRIMERA FORMA DE PONER UNA IMAGEN CON BORDES REDONDEADOS
+          // ClipRRect(
+          //   borderRadius: BorderRadius.circular(20),
+          //   child: Image.network(
+          //     "https://images.pexels.com/photos/28503598/pexels-photo-28503598.jpeg",
+          //     height: screenWidth / 4,
+          //     width: screenWidth / 4,
+          //     fit: BoxFit.cover,
+          //   ),
+          // ),
+
+          // SEGUNDA FORMA DE PONER UNA IMAGEN CON BORDES REDONDEADOS
+          Container(
+            height: screenWidth / 4,
+            width: screenWidth / 4,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(menuMap["imageUrl"]),
+              ),
+            ),
+          ),
+
+          SizedBox(width: 16),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Menu ${menuMap["id"]}", style: TextStyle(fontSize: 20)),
+              Text(
+                menuMap["days"],
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
+              ),
+              Text(
+                "S/. ${menuMap["price"]}",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,70 +108,16 @@ class MenuPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               "Selecciona tu mejor opción:",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
-            SizedBox(height: 32),
-            Container(
-              padding: EdgeInsets.all(16),
-              color: Colors.red,
-              child: Row(
-                children: [
-                  // PRIMERA FORMA DE PONER UNA IMAGEN CON BORDES REDONDEADOS
-                  // ClipRRect(
-                  //   borderRadius: BorderRadius.circular(20),
-                  //   child: Image.network(
-                  //     "https://images.pexels.com/photos/28503598/pexels-photo-28503598.jpeg",
-                  //     height: screenWidth / 4,
-                  //     width: screenWidth / 4,
-                  //     fit: BoxFit.cover,
-                  //   ),
-                  // ),
-
-                  // SEGUNDA FORMA DE PONER UNA IMAGEN CON BORDES REDONDEADOS
-                  Container(
-                    height: screenWidth / 4,
-                    width: screenWidth / 4,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                          "https://images.pexels.com/photos/28503598/pexels-photo-28503598.jpeg",
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(width: 16),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Menu 1",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                      Text(
-                        "Lun - Mar - Vier",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                      Text(
-                        "S/. 5",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+            Column(
+              children: List.generate(
+                _menuList.length,
+                (i) => _buildMenuCard(screenWidth, _menuList[i]),
               ),
             ),
           ],
